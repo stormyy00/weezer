b3db7a17c87ccfca4d8fe968e64f69be98aa9227
Jonathan Trujillo <jonathan.trujillo0432@gmail.com>
2026-01-15 23:00:12 -0800

    animated icons + icons

---
+++ src/data/socials.ts
@@ -0,0 +1,17 @@
+import { Mail } from "lucide-react";
+import { FaInstagram, FaLinkedin, FaYoutube , FaTiktok, FaDiscord} from "react-icons/fa";
+import { IoLogoFacebook } from "react-icons/io";
+import { FaXTwitter } from "react-icons/fa6";
+import { CiGlobe } from "react-icons/ci";
+
+export const SOCIALS: Record<string, React.ComponentType> = {
+    instagram: FaInstagram,
+    facebook: IoLogoFacebook,
+    twitter: FaXTwitter,
+    linkedin: FaLinkedin,
+    youtube: FaYoutube,
+    website: CiGlobe,
+    tiktok: FaTiktok,
+    discord: FaDiscord,
+    email: Mail
+}
