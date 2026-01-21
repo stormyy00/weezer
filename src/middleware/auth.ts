f928406b688780b0bc50aa36e54893af1e7fc58f
Jonathan Trujillo <jonathan.trujillo0432@gmail.com>
2026-01-20 22:53:02 -0800

    middlware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,32 +3,48 @@ import { createMiddleware } from "@tanstack/react-start";
 import { getRequestHeaders } from "@tanstack/react-start/server";
 import { auth } from "@/lib/auth";
 
-export const authMiddleware = createMiddleware().server(
-    async ({ next }) => {
-        const headers = getRequestHeaders();
-        const session = await auth.api.getSession({ headers })
+export const authMiddleware = createMiddleware().server(async ({ next }) => {
+  const headers = getRequestHeaders();
+  const session = await auth.api.getSession({ headers });
 
-        if (!session) {
-            throw redirect({ to: "/adminlogin" })
-        }
+  if (!session) {
+    throw redirect({ to: "/adminlogin" });
+  }
 
-        return await next()
-    }
-);
+  return await next();
+});
+
+export const isAdmin = createMiddleware().server(async ({ next }) => {
+  const headers = getRequestHeaders();
+  const session = await auth.api.getSession({ headers });
+
+  if (!session || session.user.role !== "admin") {
+    await auth.api.signOut({ headers });
+    console.error("User data breach attempt detected:", session?.user);
+    throw redirect({ to: "/adminlogin" });
+  }
+
+  return await next();
+});
+
+export const authenticate = createMiddleware().server(async ({ next }) => {
+  const headers = getRequestHeaders();
+  const session = await auth.api.getSession({ headers });
+
+  if (!session || session.user.role !== "admin") {
+    await auth.api.signOut({ headers });
+    throw redirect({ to: "/adminlogin" });
+  }
+
+  return await next({
+    context: {
+      userId: session.user.id,
+      user: session.user,
+    },
+  });
+});
 
 
-export const isAdmin = createMiddleware().server(
-    async ({ next }) => {
-        const headers = getRequestHeaders();
-        const session = await auth.api.getSession({ headers })
-
-    if (!session || session.user.role !== "admin") {
-        await auth.api.signOut({ headers })
-        console.error("User data breach attempt detected:", session?.user)
-        throw redirect({ to: "/adminlogin" })
-    }
-
-
-    return await next()
+export const getServerSession = async () => {
+  return await auth.api.getSession({ headers: getRequestHeaders() });
 }
-);
